998db69c60b8387dd4a1d3e3510293604ab33130
Shravali <shravali@gmail.com>
2017-09-10 15:29:15 -0400

    updated main search page, probably wont use details page for now

--- public/services/search.service.client.js
+++ public/services/search.service.client.js
@@ -19,7 +19,7 @@
         }
 
         function searchCharityById(id){
-            var url="http://data.orghunter.com/v1/charitybasic?user_key=35887bed91015b12bdfb52d38432e6d2&ein=" + id;
+            var url="https://api.data.charitynavigator.org/v2/Organizations/" + id + "?app_id=a4336653&app_key=6e0e0854264a55a46df66c7f7bf5e5aa";
             return $http.get(url)
                 .then(console.log("searching"))
                 .then(function(response) {
